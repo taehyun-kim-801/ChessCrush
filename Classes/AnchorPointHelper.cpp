a702bb2dfc1316d89b5c498f01d6bc3d124be7a1
김태현 <allone1129@gmail.com>
2020-03-17 19:52:57 +0900

    Change Helper fields to value not pointer

--- Classes/AnchorPointHelper.cpp
+++ Classes/AnchorPointHelper.cpp
@@ -1,23 +1,23 @@
 #include "AnchorPointHelper.h"
 
-Point* AnchorPointHelper::TopLeft = nullptr;
-Point* AnchorPointHelper::TopCenter = nullptr;
-Point* AnchorPointHelper::TopRight = nullptr;
-Point* AnchorPointHelper::MiddleLeft = nullptr;
-Point* AnchorPointHelper::MiddleCenter = nullptr;
-Point* AnchorPointHelper::MiddleRight = nullptr;
-Point* AnchorPointHelper::BottomLeft = nullptr;
-Point* AnchorPointHelper::BottomCenter = nullptr;
-Point* AnchorPointHelper::BottomRight = nullptr;
+Point AnchorPointHelper::TopLeft = nullptr;
+Point AnchorPointHelper::TopCenter = nullptr;
+Point AnchorPointHelper::TopRight = nullptr;
+Point AnchorPointHelper::MiddleLeft = nullptr;
+Point AnchorPointHelper::MiddleCenter = nullptr;
+Point AnchorPointHelper::MiddleRight = nullptr;
+Point AnchorPointHelper::BottomLeft = nullptr;
+Point AnchorPointHelper::BottomCenter = nullptr;
+Point AnchorPointHelper::BottomRight = nullptr;
 
 void AnchorPointHelper::Initialize() {
-	TopLeft = new Point(0, 1);
-	TopCenter = new Point(0.5f, 1);
-	TopRight = new Point(1, 1);
-	MiddleLeft = new Point(0, 0.5f);
-	MiddleCenter = new Point(0.5f, 0.5f);
-	MiddleRight = new Point(1, 0.5f);
-	BottomLeft = new Point(0, 0);
-	BottomCenter = new Point(0.5f, 0);
-	BottomRight = new Point(1, 0);
+	TopLeft = Point(0, 1);
+	TopCenter = Point(0.5f, 1);
+	TopRight = Point(1, 1);
+	MiddleLeft = Point(0, 0.5f);
+	MiddleCenter = Point(0.5f, 0.5f);
+	MiddleRight = Point(1, 0.5f);
+	BottomLeft = Point(0, 0);
+	BottomCenter = Point(0.5f, 0);
+	BottomRight = Point(1, 0);
 }
--- Classes/ScreenPositionHelper.h
+++ Classes/ScreenPositionHelper.h
@@ -4,15 +4,15 @@ USING_NS_CC;
 
 class ScreenPositionHelper {
 public:
-	static Point* TopLeft;
-	static Point* TopCenter;
-	static Point* TopRight;
-	static Point* MiddleLeft;
-	static Point* MiddleCenter;
-	static Point* MiddleRight;
-	static Point* BottomLeft;
-	static Point* BottomCenter;
-	static Point* BottomRight;
+	static Point TopLeft;
+	static Point TopCenter;
+	static Point TopRight;
+	static Point MiddleLeft;
+	static Point MiddleCenter;
+	static Point MiddleRight;
+	static Point BottomLeft;
+	static Point BottomCenter;
+	static Point BottomRight;
 
 	static void Initialize();
 };
