7546c1053156718714ce5bf30c589e4821516be2
김태현 <allone1129@gmail.com>
2020-03-17 19:36:33 +0900

    Fix LNK2005 error

--- Classes/AnchorPointHelper.h
+++ Classes/AnchorPointHelper.h
@@ -4,16 +4,15 @@ USING_NS_CC;
 
 class AnchorPointHelper {
 public:
-	static Point& TopLeft;
-	static Point& TopCenter;
-	static Point& TopRight;
-	static Point& MiddleLeft;
-	static Point& MiddleCenter;
-	static Point& MiddleRight;
-	static Point& MiddleRight;
-	static Point& BottomLeft;
-	static Point& BottomCenter;
-	static Point& BottomRight;
+	static Point* TopLeft;
+	static Point* TopCenter;
+	static Point* TopRight;
+	static Point* MiddleLeft;
+	static Point* MiddleCenter;
+	static Point* MiddleRight;
+	static Point* BottomLeft;
+	static Point* BottomCenter;
+	static Point* BottomRight;
 
 	static void Initialize();
 };
--- Classes/ScreenPositionHelper.cpp
+++ Classes/ScreenPositionHelper.cpp
@@ -1,15 +1,25 @@
 #include "ScreenPositionHelper.h"
 
+Point* ScreenPositionHelper::TopLeft = nullptr;
+Point* ScreenPositionHelper::TopCenter = nullptr;
+Point* ScreenPositionHelper::TopRight = nullptr;
+Point* ScreenPositionHelper::MiddleLeft = nullptr;
+Point* ScreenPositionHelper::MiddleCenter = nullptr;
+Point* ScreenPositionHelper::MiddleRight = nullptr;
+Point* ScreenPositionHelper::BottomLeft = nullptr;
+Point* ScreenPositionHelper::BottomCenter = nullptr;
+Point* ScreenPositionHelper::BottomRight = nullptr;
+
 void ScreenPositionHelper::Initialize() {
 	const auto screenSize = Director::getInstance()->getWinSize();
 	
-	TopLeft = Point(0, screenSize.height);
-	TopCenter = Point(screenSize.width / 2, screenSize.height);
-	TopRight = Point(screenSize.width, screenSize.height);
-	MiddleLeft = Point(0, screenSize.height / 2);
-	MiddleCenter = Point(screenSize.width / 2, screenSize.height / 2);
-	MiddleRight = Point(screenSize.width, screenSize.height / 2);
-	BottomLeft = Point::ZERO;
-	BottomCenter = Point(screenSize.width / 2, 0);
-	BottomRight = Point(screenSize.width, 0);
+	TopLeft = new Point(0, screenSize.height);
+	TopCenter = new Point(screenSize.width / 2, screenSize.height);
+	TopRight = new Point(screenSize.width, screenSize.height);
+	MiddleLeft = new Point(0, screenSize.height / 2);
+	MiddleCenter = new Point(screenSize.width / 2, screenSize.height / 2);
+	MiddleRight = new Point(screenSize.width, screenSize.height / 2);
+	BottomLeft = new Point(0, 0);
+	BottomCenter = new Point(screenSize.width / 2, 0);
+	BottomRight = new Point(screenSize.width, 0);
 }
