28ed97e51338b7562f9fd8a1d1029b09fc4e5449
김태현 <allone1129@gmail.com>
2020-03-23 23:00:02 +0900

    Implement Vector3Extensions

---
+++ Assets/_Scripts/Extensions/Vector3Extensions.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace ChessCrush
+{
+    public static class Vector3Extensions
+    {
+        private static RectTransform canvasRectTransform;
+        public static Vector3 ToCanvasPosition(this Vector3 worldPosition)
+        {
+            if (canvasRectTransform is null)
+                canvasRectTransform = MainCanvas.instance.GetComponent<RectTransform>();
+            Vector2 viewportPosition = Camera.main.WorldToViewportPoint(worldPosition);
+            Vector2 canvasPosition = new Vector2(
+                ((viewportPosition.x * canvasRectTransform.sizeDelta.x) - (canvasRectTransform.sizeDelta.x * 0.5f)),
+                ((viewportPosition.y * canvasRectTransform.sizeDelta.y) - (canvasRectTransform.sizeDelta.y * 0.5f)));
+
+            return canvasPosition;
+        }
+    }
+}
