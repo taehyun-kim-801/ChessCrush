3f0c6b6f527490c5c5c7934f2b7217bb1cde7d99
김태현 <allone1129@gmail.com>
2020-03-24 00:18:26 +0900

    Implement spawn move square prefab when click the button

---
+++ Assets/_Scripts/UI/SpawnButton.cs
@@ -0,0 +1,34 @@
+﻿using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ChessCrush.UI
+{
+    public class SpawnButton: MonoBehaviour
+    {
+        public Button button;
+        public SpawnType spawnType;
+
+        private void Awake()
+        {
+            button.OnClickAsObservable().Subscribe(_ => SubscribeButton());
+        }
+
+        private void SubscribeButton()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                if (Game.instance.chessBoard.AnybodyIn(i, 0))
+                {
+                    var square = Game.instance.objectPool.Use("DisableMoveSquare");
+                    square.GetComponent<ChessBoardObject>().Initialize(i, 0);
+                }
+                else
+                {
+                    var square = Game.instance.objectPool.Use("AbleMoveSquare");
+                    square.GetComponent<AbleMoveSquare>().Initialize(i, 0);
+                }
+            }
+        }
+    }
+}
