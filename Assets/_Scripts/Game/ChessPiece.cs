b01e12e5933aaf50fd144fe83f6a64bdef5c874e
김태현 <allone1129@gmail.com>
2020-03-25 00:19:15 +0900

    Implement ChessPiece

--- Assets/_Scripts/Game/ChessPiece.cs
+++ Assets/_Scripts/Game/ChessPiece.cs
@@ -1,10 +1,29 @@
 ﻿namespace ChessCrush.Game
 {
-    public abstract class ChessPiece
+    public class ChessPiece
     {
         public int x { get; private set; }
         public int y { get; private set; }
+        public PieceType pieceType { get; private set; }
 
-        public abstract void MoveTo(int x, int y);
+        public ChessPiece() { }
+        public ChessPiece(int x,int y,PieceType pieceType)
+        {
+            this.x = x;
+            this.y = y;
+            this.pieceType = pieceType;
+        }
+        public ChessPiece(ChessPiece other)
+        {
+            x = other.x;
+            y = other.y;
+            pieceType = other.pieceType;
+        }
+
+        public void MoveTo(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
     }
 }
