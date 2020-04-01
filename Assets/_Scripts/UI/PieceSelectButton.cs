3fbbf0ee9a89dced218736e36175fdeba4fd34ad
김태현 <allone1129@gmail.com>
2020-04-01 16:17:13 +0900

    Implement PieceSelectButton

---
+++ Assets/_Scripts/UI/PieceSelectButton.cs
@@ -0,0 +1,141 @@
+﻿using ChessCrush.Game;
+using UniRx;
+using UnityEngine.UI;
+
+namespace ChessCrush.UI
+{
+    public class PieceSelectButton: ChessBoardObject
+    {
+        private Button button;
+        private PieceType pieceType;
+        private ChessBoardVector chessBoardPosition = new ChessBoardVector();
+        private static ChessGameDirector chessGameDirector;
+
+        private void Awake()
+        {
+            button = GetComponent<Button>();
+            button.OnClickAsObservable().Subscribe(_ => SubscribeButton()).AddTo(gameObject);
+        }
+
+        private void Start()
+        {
+            chessGameDirector = Director.instance.GetSubDirector<ChessGameDirector>();
+        }
+
+        private void SubscribeButton()
+        {
+            switch(pieceType)
+            {
+                case PieceType.Pawn:
+                    UsePawnMoveSquare();
+                    return;
+                case PieceType.Bishop:
+                    UseBishopMoveSquare();
+                    return;
+                case PieceType.Knight:
+                    UseKnightMoveSquare();
+                    return;
+                case PieceType.Rook:
+                    UseRookMoveSquare();
+                    return;
+                case PieceType.Queen:
+                    UseQueenMoveSquare();
+                    return;
+                case PieceType.King:
+                    UseKingMoveSquare();
+                    return;
+                default:
+                    return;
+            }
+        }
+
+        #region Use MoveSquare
+        private void UsePawnMoveSquare()
+        {
+            UseMoveSquare(new ChessBoardVector(chessBoardPosition.x,chessBoardPosition.y + 1));
+            if (chessGameDirector.chessGameObjects.chessBoard.AnybodyIn(chessBoardPosition.x - 1, chessBoardPosition.y + 1))
+                UseMoveSquare(new ChessBoardVector(chessBoardPosition.x - 1, chessBoardPosition.y + 1));
+            if (chessGameDirector.chessGameObjects.chessBoard.AnybodyIn(chessBoardPosition.x + 1, chessBoardPosition.y + 1))
+                UseMoveSquare(new ChessBoardVector(chessBoardPosition.x + 1, chessBoardPosition.y + 1));
+        }
+
+        private void UseBishopMoveSquare()
+        {
+            UseMoveSquare(new ChessBoardVector(chessBoardPosition.x + 2, chessBoardPosition.y + 2));
+            UseMoveSquare(new ChessBoardVector(chessBoardPosition.x + 1, chessBoardPosition.y + 1));
+            UseMoveSquare(new ChessBoardVector(chessBoardPosition.x - 1, chessBoardPosition.y + 1));
+            UseMoveSquare(new ChessBoardVector(chessBoardPosition.x - 2, chessBoardPosition.y + 2));
+            UseMoveSquare(new ChessBoardVector(chessBoardPosition.x - 1, chessBoardPosition.y - 1));
+            UseMoveSquare(new ChessBoardVector(chessBoardPosition.x - 2, chessBoardPosition.y - 2));
+            UseMoveSquare(new ChessBoardVector(chessBoardPosition.x + 1, chessBoardPosition.y - 1));
+            UseMoveSquare(new ChessBoardVector(chessBoardPosition.x + 2, chessBoardPosition.y - 2));
+        }
+
+        private void UseKnightMoveSquare()
+        {
+            UseMoveSquare(new ChessBoardVector(chessBoardPosition.x + 2, chessBoardPosition.y + 1));
+            UseMoveSquare(new ChessBoardVector(chessBoardPosition.x + 1, chessBoardPosition.y + 2));
+            UseMoveSquare(new ChessBoardVector(chessBoardPosition.x - 2, chessBoardPosition.y + 1));
+            UseMoveSquare(new ChessBoardVector(chessBoardPosition.x - 1, chessBoardPosition.y + 2));
+            UseMoveSquare(new ChessBoardVector(chessBoardPosition.x - 2, chessBoardPosition.y - 1));
+            UseMoveSquare(new ChessBoardVector(chessBoardPosition.x - 1, chessBoardPosition.y - 2));
+            UseMoveSquare(new ChessBoardVector(chessBoardPosition.x + 2, chessBoardPosition.y - 1));
+            UseMoveSquare(new ChessBoardVector(chessBoardPosition.x + 1, chessBoardPosition.y - 2));
+        }
+
+        private void UseRookMoveSquare()
+        {
+            UseMoveSquare(new ChessBoardVector(chessBoardPosition.x + 2, chessBoardPosition.y));
+            UseMoveSquare(new ChessBoardVector(chessBoardPosition.x + 1, chessBoardPosition.y));
+            UseMoveSquare(new ChessBoardVector(chessBoardPosition.x - 2, chessBoardPosition.y));
+            UseMoveSquare(new ChessBoardVector(chessBoardPosition.x - 1, chessBoardPosition.y));
+            UseMoveSquare(new ChessBoardVector(chessBoardPosition.x, chessBoardPosition.y + 2));
+            UseMoveSquare(new ChessBoardVector(chessBoardPosition.x, chessBoardPosition.y + 1));
+            UseMoveSquare(new ChessBoardVector(chessBoardPosition.x, chessBoardPosition.y - 2));
+            UseMoveSquare(new ChessBoardVector(chessBoardPosition.x, chessBoardPosition.y - 1));
+        }
+
+        private void UseQueenMoveSquare()
+        {
+            UseRookMoveSquare();
+            UseBishopMoveSquare();
+        }
+
+        private void UseKingMoveSquare()
+        {
+            UseMoveSquare(new ChessBoardVector(chessBoardPosition.x + 1, chessBoardPosition.y));
+            UseMoveSquare(new ChessBoardVector(chessBoardPosition.x - 1, chessBoardPosition.y));
+            UseMoveSquare(new ChessBoardVector(chessBoardPosition.x, chessBoardPosition.y + 1));
+            UseMoveSquare(new ChessBoardVector(chessBoardPosition.x, chessBoardPosition.y - 1));
+            UseMoveSquare(new ChessBoardVector(chessBoardPosition.x + 1, chessBoardPosition.y + 1));
+            UseMoveSquare(new ChessBoardVector(chessBoardPosition.x - 1, chessBoardPosition.y + 1));
+            UseMoveSquare(new ChessBoardVector(chessBoardPosition.x - 1, chessBoardPosition.y - 1));
+            UseMoveSquare(new ChessBoardVector(chessBoardPosition.x + 1, chessBoardPosition.y - 1));
+        }
+
+        private void UseMoveSquare(ChessBoardVector position)
+        {
+            if (position.x < 0 || position.y < 0 || position.x > 7 || position.y > 7)
+                return;
+
+            if (chessGameDirector.chessGameObjects.chessBoard.AnybodyIn(position.x, position.y))
+                DisableMoveSquare.UseWithComponent(position);
+            else
+                AbleMoveSquare.UseWithComponent(position);
+        }
+        #endregion
+
+        private void Initialize(ChessBoardVector position,PieceType pieceType)
+        {
+            chessBoardPosition = position;
+            this.pieceType = pieceType;
+        }
+
+        public static PieceSelectButton UseWithComponent(ChessBoardVector position,PieceType pieceType)
+        {
+            var result = chessGameDirector.chessGameUI.objectPool.Use("PieceSelectButton").GetComponent<PieceSelectButton>();
+            result.Initialize(position, pieceType);
+            return result;
+        }
+    }
+}
