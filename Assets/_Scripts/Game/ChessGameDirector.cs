c34ccf500a5827657293c52677da9512a5dbabbf
김태현 <allone1129@gmail.com>
2020-04-02 11:50:40 +0900

    Implement game flow in ChessGameDirector

--- Assets/_Scripts/Game/ChessGameDirector.cs
+++ Assets/_Scripts/Game/ChessGameDirector.cs
@@ -1,13 +1,19 @@
 ﻿using ChessCrush.UI;
+using System;
+using System.Collections;
 using UnityEngine;
 
 namespace ChessCrush.Game
 {
-    public class ChessGameDirector: SubDirector
+    public class ChessGameDirector : SubDirector
     {
         //TODO: Player, Enemy Player field
+        [NonSerialized]
         public ChessGameObjects chessGameObjects;
+        [NonSerialized]
         public ChessGameUI chessGameUI;
+        [SerializeField]
+        private float InputTime;
 
         private void OnEnable()
         {
@@ -15,6 +21,32 @@ namespace ChessCrush.Game
             chessGameObjects = cgo.GetComponent<ChessGameObjects>();
             var cgUI = MainCanvas.instance.Use(nameof(ChessGameUI));
             chessGameUI = cgUI.GetComponent<ChessGameUI>();
+            StartCoroutine(CoGamePlay());
+        }
+
+        private IEnumerator CoGamePlay()
+        {
+            yield return StartCoroutine(CoSetAttackPlayer());
+            while (true)
+            {
+                yield return StartCoroutine(CoInput());
+                yield return StartCoroutine(CoSimulate());
+            }
+        }
+
+        private IEnumerator CoSetAttackPlayer()
+        {
+            yield return null;
+        }
+
+        private IEnumerator CoInput()
+        {
+            yield return new WaitForSeconds(InputTime);
+        }
+
+        private IEnumerator CoSimulate()
+        {
+            yield return null;
         }
     }
 }
