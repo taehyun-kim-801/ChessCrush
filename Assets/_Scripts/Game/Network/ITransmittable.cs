f35908275445d4ee18314af844f510ab3202181d
김태현 <allone1129@gmail.com>
2020-04-06 17:59:04 +0900

    Implementt ITransmittable

---
+++ Assets/_Scripts/Game/Network/ITransmittable.cs
@@ -0,0 +1,13 @@
+﻿namespace ChessCrush.Game
+{
+    public interface ITransmittable
+    {
+        int NetworkID { get; set; }
+
+        void Write(OutputMemoryStream outputMemoryStream);
+        void Write(OutputMemoryBitStream outputMemoryBitStream);
+
+        void Read(InputMemoryStream inputMemoryStream);
+        void Read(InputMemoryBitStream inputMemoryBitStream);
+    }
+}
