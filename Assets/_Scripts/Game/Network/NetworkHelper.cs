c631ba9b2082d5e544a2452b6fcab65e51cc1449
김태현 <allone1129@gmail.com>
2020-04-09 17:18:53 +0900

    Define NetworkHelper

---
+++ Assets/_Scripts/Game/Network/NetworkHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace ChessCrush.Game
+{
+    public class NetworkHelper
+    {
+        private static string serverIPString;
+        private static int port;
+
+        private Socket socket;
+        public bool socketConnected { get { return socket.Connected; } }
+
+        public NetworkHelper() 
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(new IPEndPoint(IPAddress.Any, 0));
+            socket.Connect(IPAddress.Parse(serverIPString), port);
+        }
+    }
+}
