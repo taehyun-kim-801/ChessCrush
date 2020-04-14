bb066f5f970da2f381cf474626817f1ef377fc2a
김태현 <allone1129@gmail.com>
2020-04-14 23:25:42 +0900

    Implement opeartion result codes

---
+++ Assets/_Scripts/Types/OperationResultCode.cs
@@ -0,0 +1,7 @@
+﻿namespace ChessCrush.OperationResultCode
+{
+    public enum SignUpCode { Success, UsingID }
+    public enum SignInCode { }
+    public enum ParticipateCode { }
+    public enum SendActionCode { }
+}
