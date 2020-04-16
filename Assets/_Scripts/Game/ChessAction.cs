301990a5cdaabd188a77a20db0864cbbf3978980
김태현 <allone1129@gmail.com>
2020-04-16 18:58:25 +0900

    Implement ChessAction

---
+++ Assets/_Scripts/Game/ChessAction.cs
@@ -0,0 +1,27 @@
+﻿namespace ChessCrush.Game
+{
+    public class ChessAction: ITransmittable
+    {
+        public int pieceId;
+        public PieceType pieceType;
+        public ChessBoardVector chessBoardVector;
+
+        public void Read(InputMemoryStream inputMemoryStream)
+        {
+            inputMemoryStream.Read(out pieceId);
+            inputMemoryStream.Read(out int typeRes);
+            pieceType = (PieceType)typeRes;
+            inputMemoryStream.Read(out int x);
+            inputMemoryStream.Read(out int y);
+            chessBoardVector = new ChessBoardVector(x, y);
+        }
+
+        public void Write(OutputMemoryStream outputMemoryStream)
+        {
+            outputMemoryStream.Write(pieceId);
+            outputMemoryStream.Write((int)pieceType);
+            outputMemoryStream.Write(chessBoardVector.x);
+            outputMemoryStream.Write(chessBoardVector.y);
+        }
+    }
+}
