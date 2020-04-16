4f87e7bc748d675be526deb70468cdffdec1fde0
김태현 <allone1129@gmail.com>
2020-04-16 18:51:52 +0900

    Delete useless method in ITransmittable

--- Assets/_Scripts/Game/Network/ITransmittable.cs
+++ Assets/_Scripts/Game/Network/ITransmittable.cs
@@ -2,12 +2,7 @@
 {
     public interface ITransmittable
     {
-        int NetworkID { get; set; }
-
         void Write(OutputMemoryStream outputMemoryStream);
-        void Write(OutputMemoryBitStream outputMemoryBitStream);
-
         void Read(InputMemoryStream inputMemoryStream);
-        void Read(InputMemoryBitStream inputMemoryBitStream);
     }
 }
