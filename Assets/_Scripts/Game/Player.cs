d3341218d98120f8355eb0b76d07d522d072fb95
김태현 <allone1129@gmail.com>
2020-04-20 17:14:11 +0900

    Change player field to ReactiveProperty

--- Assets/_Scripts/Game/Player.cs
+++ Assets/_Scripts/Game/Player.cs
@@ -1,27 +1,55 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using UniRx;
 
 namespace ChessCrush.Game
 {
-    public class Player
+    public class Player: IDisposable
     {
-        private string name;
-        private bool isWhite;
-        private int hp;
-        private int energyPoint;
-        public List<ChessAction> chessActions;
+        private ReactiveProperty<string> name = new ReactiveProperty<string>();
+        private ReactiveProperty<bool> isWhite = new ReactiveProperty<bool>();
+        private ReactiveProperty<int> hp = new ReactiveProperty<int>();
+        private ReactiveProperty<int> energyPoint = new ReactiveProperty<int>();
+        private bool isMe;
+        public ReactiveProperty<List<ChessAction>> chessActions = new ReactiveProperty<List<ChessAction>>();
 
         public Player()
         {
-            chessActions = new List<ChessAction>();
         }
 
-        public Player(string name, bool isWhite)
+        public Player(string name, bool isWhite, bool isMe)
         {
-            this.name = name;
-            this.isWhite = isWhite;
-            hp = 20;
-            energyPoint = 0;
-            chessActions = new List<ChessAction>();
+            this.name.Value = name;
+            this.isWhite.Value = isWhite;
+            hp.Value = 20;
+            energyPoint.Value = 0;
+            this.isMe = isMe;
+
+            var gameDirector = Director.instance.GetSubDirector<ChessGameDirector>();
+            if (isMe)
+            {
+                this.name.Subscribe(str => gameDirector.chessGameUI.myStatus.nameText.text = $"Player: {str}");
+                //TODO: isWhite Subscribe
+                hp.Subscribe(_ => gameDirector.chessGameUI.myStatus.hpBar.fillAmount = _ / 20);
+                energyPoint.Subscribe(_ => gameDirector.chessGameUI.myStatus.energyBar.fillAmount = _ / 10);
+                //TODO: chessActions Subscribe
+            }
+            else
+            {
+                this.name.Subscribe(str => gameDirector.chessGameUI.enemyStatus.nameText.text = $"Player:{str}");
+                //TODO: isWhite Subscribe
+                hp.Subscribe(_ => gameDirector.chessGameUI.enemyStatus.hpBar.fillAmount = _ / 20);
+                energyPoint.Subscribe(_ => gameDirector.chessGameUI.enemyStatus.energyBar.fillAmount = _ / 10);
+            }
+        }
+
+        public void Dispose()
+        {
+            name.Dispose();
+            isWhite.Dispose();
+            hp.Dispose();
+            energyPoint.Dispose();
+            chessActions.Dispose();
         }
     }
 }
