4f4e5ca462a749bf391d137225f530f833ab5e41
김태현 <allone1129@gmail.com>
2020-05-08 10:17:44 +0900

    Implement FriendsWidget

---
+++ Assets/_Scripts/UI/FriendsWidget.cs
@@ -0,0 +1,28 @@
+﻿using BackEnd;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ChessCrush.UI
+{
+    public class FriendsWidget: MonoBehaviour
+    {
+        [SerializeField]
+        private Button background;
+        [SerializeField]
+        private GameObject myFriendsScrollContent;
+        [SerializeField]
+        private Button goToSearchButton;
+        [SerializeField]
+        private GameObject requestWidget;
+        [SerializeField]
+        private Button exitButton;
+
+        private void Awake()
+        {
+            background.OnClickAsObservable().Subscribe(_ => gameObject.SetActive(false));
+            goToSearchButton.OnClickAsObservable().Subscribe(_ => requestWidget.SetActive(true));
+            exitButton.OnClickAsObservable().Subscribe(_ => gameObject.SetActive(false));
+        }
+    }
+}
