ee75bf925e64d78e5a9b1d21b3637558acc849c4
김태현 <allone1129@gmail.com>
2020-05-11 20:13:37 +0900

    Implement FriendsView

---
+++ Assets/_Scripts/UI/FriendsView.cs
@@ -0,0 +1,41 @@
+﻿using BackEnd;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ChessCrush.UI
+{
+    public class FriendsView:MonoBehaviour
+    {
+        [SerializeField]
+        private Text nicknameText;
+        [SerializeField]
+        private Button chatButton;
+        [SerializeField]
+        private Button breakButton;
+
+        private string friendInDate;
+
+        private void Awake()
+        {
+            chatButton.OnClickAsObservable().Subscribe(_ => SubscribeChatButton()).AddTo(gameObject);
+            breakButton.OnClickAsObservable().Subscribe(_ => SubscribeBreakButton()).AddTo(gameObject);
+        }
+
+        public void Set(string nickname, string inDate)
+        {
+            nicknameText.text = nickname;
+            friendInDate = inDate;
+        }
+
+        private void SubscribeChatButton()
+        {
+            
+        }
+
+        private void SubscribeBreakButton()
+        {
+            Backend.Social.Friend.BreakFriend(friendInDate);
+        }
+    }
+}
