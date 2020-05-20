fe783100ec926d7c982a5b4135c2c0d9a05b6043
김태현 <allone1129@gmail.com>
2020-05-20 13:59:40 +0900

    Implement backend initialize in BackendDirector

---
+++ Assets/_Scripts/Game/BackendDirector.cs
@@ -0,0 +1,23 @@
+﻿using BackEnd;
+using ChessCrush.UI;
+using UniRx;
+using UniRx.Triggers;
+
+namespace ChessCrush.Game
+{
+    public class BackendDirector: SubDirector
+    {
+        private string roomToken;
+
+        private void Awake()
+        {
+            Backend.Initialize(() =>
+            {
+                if (Backend.IsInitialized)
+                    gameObject.UpdateAsObservable().Subscribe(_ => Backend.Match.poll()).AddTo(gameObject);
+                else
+                    MessageBoxUI.UseWithComponent("Failed to connect to server");
+            });
+        }
+    }
+}
