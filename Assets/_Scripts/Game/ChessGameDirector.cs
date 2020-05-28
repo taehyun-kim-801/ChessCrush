820e5a76b1e0a6f481042ca787abb2cc7f55b6db
김태현 <allone1129@gmail.com>
2020-05-28 20:21:01 +0900

    Add attack icon

--- Assets/_Scripts/Game/ChessGameDirector.cs
+++ Assets/_Scripts/Game/ChessGameDirector.cs
@@ -1,6 +1,7 @@
 ﻿using ChessCrush.UI;
 using System;
 using System.Collections;
+using UniRx;
 using UnityEngine;
 
 namespace ChessCrush.Game
@@ -18,6 +19,8 @@ namespace ChessCrush.Game
 
         public Player player;
         public Player enemyPlayer;
+
+        public ReactiveProperty<int> turnCount = new ReactiveProperty<int>();
 
         private void OnEnable()
         {
@@ -44,6 +47,7 @@ namespace ChessCrush.Game
             yield return new WaitWhile(() => player is null || enemyPlayer is null);
             while (true)
             {
+                turnCount.Value++;
                 yield return StartCoroutine(CoInput());
                 yield return StartCoroutine(CoSimulate());
             }
--- Assets/_Scripts/Game/Player.cs
+++ Assets/_Scripts/Game/Player.cs
@@ -8,11 +8,14 @@ namespace ChessCrush.Game
     {
         private ReactiveProperty<string> name = new ReactiveProperty<string>();
         private ReactiveProperty<bool> isWhite = new ReactiveProperty<bool>();
+        public bool IsWhite { get { return isWhite.Value; } }
         private ReactiveProperty<int> hp = new ReactiveProperty<int>();
         private ReactiveProperty<int> energyPoint = new ReactiveProperty<int>();
         public bool IsMe { get; private set; }
         public List<ChessAction> chessActions = new List<ChessAction>();
         public Subject<List<ChessAction>> actionsSubject = new Subject<List<ChessAction>>();
+
+        private ChessGameDirector chessGameDirector;
 
         public Player()
         {
@@ -27,37 +30,37 @@ namespace ChessCrush.Game
             chessActions = new List<ChessAction>();
             this.IsMe = isMe;
 
-            var gameDirector = Director.instance.GetSubDirector<ChessGameDirector>();
+            chessGameDirector = Director.instance.GetSubDirector<ChessGameDirector>();
             if (isMe)
             {
-                this.name.Subscribe(str => gameDirector.chessGameUI.myStatus.nameText.text = $"Player: {str}");
+                this.name.Subscribe(str => chessGameDirector.chessGameUI.myStatus.nameText.text = $"Player: {str}");
                 //TODO: isWhite Subscribe
                 hp.Subscribe(_ =>
                 {
-                    gameDirector.chessGameUI.myStatus.hpText.text = $"{_} / 20";
-                    gameDirector.chessGameUI.myStatus.hpBar.fillAmount = _ / 20;
+                    chessGameDirector.chessGameUI.myStatus.hpText.text = $"{_} / 20";
+                    chessGameDirector.chessGameUI.myStatus.hpBar.fillAmount = _ / 20;
                 });
                 energyPoint.Subscribe(_ =>
                 {
-                    gameDirector.chessGameUI.myStatus.energyText.text = $"{_} / 10";
-                    gameDirector.chessGameUI.myStatus.energyBar.fillAmount = _ / 10;
+                    chessGameDirector.chessGameUI.myStatus.energyText.text = $"{_} / 10";
+                    chessGameDirector.chessGameUI.myStatus.energyBar.fillAmount = _ / 10;
                 });
-                actionsSubject.Subscribe(list => gameDirector.chessGameUI.chessActionScroll.SetView(list));
-                actionsSubject.Subscribe(list => gameDirector.chessGameObjects.SetExpectedAction(list));
+                actionsSubject.Subscribe(list => chessGameDirector.chessGameUI.chessActionScroll.SetView(list));
+                actionsSubject.Subscribe(list => chessGameDirector.chessGameObjects.SetExpectedAction(list));
             }
             else
             {
-                this.name.Subscribe(str => gameDirector.chessGameUI.enemyStatus.nameText.text = $"Player: {str}");
+                this.name.Subscribe(str => chessGameDirector.chessGameUI.enemyStatus.nameText.text = $"Player: {str}");
                 //TODO: isWhite Subscribe
                 hp.Subscribe(_ =>
                 {
-                    gameDirector.chessGameUI.enemyStatus.hpText.text = $"{_} / 20";
-                    gameDirector.chessGameUI.enemyStatus.hpBar.fillAmount = _ / 20;
+                    chessGameDirector.chessGameUI.enemyStatus.hpText.text = $"{_} / 20";
+                    chessGameDirector.chessGameUI.enemyStatus.hpBar.fillAmount = _ / 20;
                 });
                 energyPoint.Subscribe(_ =>
                 {
-                    gameDirector.chessGameUI.enemyStatus.energyText.text = $"{_} / 10";
-                    gameDirector.chessGameUI.enemyStatus.energyBar.fillAmount = _ / 10;
+                    chessGameDirector.chessGameUI.enemyStatus.energyText.text = $"{_} / 10";
+                    chessGameDirector.chessGameUI.enemyStatus.energyBar.fillAmount = _ / 10;
                 });
             }
         }
