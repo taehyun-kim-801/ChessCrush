437c6229ea1ffa9cd6fbbbf9d82075d9e66f3ade
김태현 <allone1129@gmail.com>
2020-05-29 11:07:09 +0900

    Implement turn text and input time circle

---
+++ Assets/_Scripts/UI/InputTimeCircle.cs
@@ -0,0 +1,34 @@
+﻿using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ChessCrush.UI
+{
+    public class InputTimeCircle: MonoBehaviour
+    {
+        [SerializeField]
+        private Image inputTimeCircle;
+        [SerializeField]
+        private Text timeText;
+
+        private float enableTime;
+
+        private void Awake()
+        {
+            inputTimeCircle.UpdateAsObservable().Subscribe(_ =>
+            {
+                inputTimeCircle.fillAmount = (30f - (Time.time - enableTime)) / 30f;
+                if (inputTimeCircle.fillAmount <= 0)
+                    gameObject.SetActive(false);
+            }).AddTo(gameObject);
+
+            timeText.UpdateAsObservable().Subscribe(_ => timeText.text = (30 - ((int)(Time.time - enableTime))).ToString()).AddTo(gameObject);
+        }
+
+        private void OnEnable()
+        {
+            enableTime = Time.time;
+        }
+    }
+}
